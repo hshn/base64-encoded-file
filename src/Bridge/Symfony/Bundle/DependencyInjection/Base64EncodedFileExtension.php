274b272fbf5e345aab6405e9c0c2a1e706664b6a
Vincent Chalamon <vincentchalamon@gmail.com>
2019-11-05 11:31:04 +0100

    Fix bundle file path

--- src/Bridge/Symfony/Bundle/DependencyInjection/Base64EncodedFileExtension.php
+++ src/Bridge/Symfony/Bundle/DependencyInjection/Base64EncodedFileExtension.php
@@ -17,7 +17,7 @@ final class Base64EncodedFileExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../../Resources/config'));
         $loader->load('services.xml');
     }
 }
