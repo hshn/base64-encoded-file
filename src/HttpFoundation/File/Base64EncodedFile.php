523b295ce5126895b3d2ae180a0079261bf9ee0e
Aurélien Bonanni <abonanni@teicee.com>
2025-06-30 23:16:16 +0200

    ref #38 - correct trailing char

--- src/HttpFoundation/File/Base64EncodedFile.php
+++ src/HttpFoundation/File/Base64EncodedFile.php
@@ -53,7 +53,7 @@ class Base64EncodedFile extends File
                 throw new FileException(sprintf('Unable to create a file into the "%s" directory', $path));
             }
 
-            if( is_array($meta) && $array_key_exists('mediatype', $meta) && $meta['mediatype'] !== null ) {
+            if( is_array($meta) && array_key_exists('mediatype', $meta) && $meta['mediatype'] !== null ) {
                 if (null !== $extension = (MimeTypes::getDefault()->getExtensions($meta['mediatype'])[0] ?? null)) {
                     $path .= '.' . $extension;
                 }
